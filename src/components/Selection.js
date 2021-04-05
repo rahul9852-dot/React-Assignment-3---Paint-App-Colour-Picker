f1397d1166419d9952b82f8603a400b36f8a9454
rahul9852-dot <66193581+rahul9852-dot@users.noreply.github.com>
2021-04-06 01:35:04 +0530

    newly updated

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,15 +1,15 @@
-import React from "react";
-import "../styles/App.css";
-const ColourSelector = (props) => {
-  const { config, selectNextBackground } = props;
-  const { background } = config;
+
+import React, { useState } from "react";
+import "../styles/Child.css";
+const Selection = ({ applyColor }) => {
+  const [color, setColor] = useState({ background: "" });
+  const handleClick = () => {
+    applyColor(setColor);
+  };
   return (
-    <button
-      className={config.classname}
-      onClick={() => selectNextBackground({ background: background })}
-    >
-      {config.label}
-    </button>
+    <div className="fix-box" style={color} onClick={handleClick}>
+      <h2 className="subheading">Selection</h2>
+    </div>
   );
 };
-export default ColourSelector;
+export default Selection;
